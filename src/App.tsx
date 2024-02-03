e56372064f2a51a66a3487f5611ed23c992c7918
Aditya Wiraha <squalvj@gmail.com>
2024-02-03 22:00:19 +0700

    feat: add leva control

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { useRef, useState } from "react";
 import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
 import "./App.css";
 
 function Box(props: ThreeElements["mesh"]) {
   const meshRef = useRef<THREE.Mesh>(null!);
   const [hovered, setHover] = useState(false);
+  const [{ size }, set] = useControls(() => ({ size: 1 }));
 
-  const [size, setSize] = useState(1);
   useFrame((state, delta) => (meshRef.current.rotation.x += delta));
   return (
     <mesh
       {...props}
       ref={meshRef}
       scale={size}
-      onClick={(event) => setSize((prev) => prev + 1)}
+      onClick={(event) => set({ size: size + 1 })}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
