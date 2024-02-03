80617bcf0ad59cf98cd9651d10dd9d2cad80d7f5
Aditya Wiraha <squalvj@gmail.com>
2024-02-03 23:21:06 +0700

    add isometric camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useMemo, useRef, useState } from "react";
-import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Canvas, ThreeElements, useFrame, useThree } from "@react-three/fiber";
 import { useControls } from "leva";
 import { ECS } from "./lib/state";
 import "./App.css";
@@ -7,8 +7,10 @@ import Player from "./GameObjects/Player/player";
 import {
   KeyboardControls,
   KeyboardControlsEntry,
+  OrthographicCamera,
   PerspectiveCamera,
 } from "@react-three/drei";
+import { OrthographicCamera as OrthographicCameraType } from "three";
 
 enum Controls {
   forward = "forward",
@@ -19,6 +21,7 @@ enum Controls {
 }
 
 const movingEntities = ECS.world.with("position", "velocity");
+const GRID_SIZE = 20;
 
 const MovementSystem = () => {
   useFrame((_, dt) => {
@@ -38,7 +41,7 @@ function Box(props: ThreeElements["mesh"]) {
   const [hovered, setHover] = useState(false);
   const [{ size }, set] = useControls(() => ({ size: 1 }));
 
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  // useFrame((state, delta) => (meshRef.current.rotation.x += delta));
 
   MovementSystem();
 
@@ -60,6 +63,49 @@ function Box(props: ThreeElements["mesh"]) {
   );
 }
 
+const Camera = (props) => {
+  const {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // setDefaultCamera,
+    size: { width, height },
+  } = useThree();
+
+  const camera = useRef<OrthographicCameraType>(null!);
+
+  // Aspect ratio depending on window size taller or wider
+  const aspect = width > height ? height / width : width / height;
+  const [zoom, setZoom] = useState(aspect * 50);
+
+  useEffect(() => {
+    // void setDefaultCamera(camera.current);
+    camera.current.rotation.order = "YXZ";
+    camera.current.rotation.y = -Math.PI / 4;
+    camera.current.rotation.x = Math.atan(-1 / Math.sqrt(2));
+    camera.current.translateZ(100);
+
+    window.addEventListener("wheel", (e) => {
+      // Limit zoom dimensions
+      if (e.deltaY < 0 && camera.current.zoom <= aspect * 300)
+        setZoom(camera.current.zoom - (e.deltaY / 120) * 10);
+      if (e.deltaY > 0 && camera.current.zoom >= aspect * 50)
+        setZoom(camera.current.zoom - (e.deltaY / 120) * 10);
+    });
+  }, []);
+
+  return (
+    <OrthographicCamera
+      makeDefault
+      ref={camera}
+      zoom={zoom}
+      near={0.1}
+      far={500}
+      rotation={[Math.atan(-1 / Math.sqrt(2)), -Math.PI / 4, 0]}
+      onUpdate={(self) => self.updateProjectionMatrix()}
+      {...props}
+    />
+  );
+};
+
 function App() {
   const map = useMemo<KeyboardControlsEntry<Controls>[]>(
     () => [
@@ -75,16 +121,15 @@ function App() {
     <div style={{ width: "100%", height: "100%" }}>
       <KeyboardControls map={map}>
         <Canvas>
-          <PerspectiveCamera makeDefault>
-            <ambientLight intensity={Math.PI / 2} />
-            <pointLight
-              position={[-10, -10, -10]}
-              decay={0}
-              intensity={Math.PI}
-            />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-          </PerspectiveCamera>
+          <Camera />
+          <ambientLight intensity={Math.PI / 2} />
+          <pointLight
+            position={[-10, -10, -10]}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
         </Canvas>
       </KeyboardControls>
     </div>
