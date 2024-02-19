5120ae686e00fa597036560ee14e367506de0729
Aditya Wiraha <squalvj@gmail.com>
2024-02-19 22:26:26 +0700

    init Stage

--- src/App.tsx
+++ src/App.tsx
@@ -1,137 +1,56 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-import { Canvas, ThreeElements, useFrame, useThree } from "@react-three/fiber";
-import { useControls } from "leva";
-import { ECS } from "./lib/state";
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
-import Player from "./GameObjects/Player/player";
 import {
-  KeyboardControls,
-  KeyboardControlsEntry,
-  OrthographicCamera,
-  PerspectiveCamera,
+  CameraControls,
+  Center,
+  Environment,
+  Grid,
+  Sky,
 } from "@react-three/drei";
-import { OrthographicCamera as OrthographicCameraType } from "three";
+import Model from "./Store1.jsx";
 
-enum Controls {
-  forward = "forward",
-  back = "back",
-  left = "left",
-  right = "right",
-  jump = "jump",
+function Ground() {
+  const gridConfig = {
+    cellSize: 0.5,
+    cellThickness: 0.5,
+    cellColor: "#000000",
+    sectionSize: 3,
+    sectionThickness: 1,
+    sectionColor: "#9d4b4b",
+    fadeDistance: 30,
+    fadeStrength: 1,
+    followCamera: false,
+  };
+  return <Grid position={[0, -0.01, 0]} args={[10.5, 10.5]} {...gridConfig} />;
 }
 
-const movingEntities = ECS.world.with("position", "velocity");
-const GRID_SIZE = 20;
-
-const MovementSystem = () => {
-  useFrame((_, dt) => {
-    for (const entity of movingEntities) {
-      console.log(entity);
-      entity.position.x += entity.velocity.x * dt;
-      entity.position.y += entity.velocity.y * dt;
-      entity.position.z += entity.velocity.z * dt;
-    }
-  });
-
-  return null;
-};
-
-function Box(props: ThreeElements["mesh"]) {
-  const meshRef = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [{ size }, set] = useControls(() => ({ size: 1 }));
-
-  // useFrame((state, delta) => (meshRef.current.rotation.x += delta));
-
-  MovementSystem();
-
+const Comp = () => {
   return (
     <>
-      <Player />
-      <mesh
-        {...props}
-        ref={meshRef}
-        scale={size}
-        onClick={(event) => set({ size: size + 1 })}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
-      >
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-      </mesh>
+      <ambientLight />
+      <CameraControls />
+      <Center top>
+        <directionalLight castShadow position-z={0.5} intensity={10} />
+        <Model />
+        <Environment preset="apartment" />
+      </Center>
     </>
-  );
-}
-
-const Camera = (props) => {
-  const {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // setDefaultCamera,
-    size: { width, height },
-  } = useThree();
-
-  const camera = useRef<OrthographicCameraType>(null!);
-
-  // Aspect ratio depending on window size taller or wider
-  const aspect = width > height ? height / width : width / height;
-  const [zoom, setZoom] = useState(aspect * 50);
-
-  useEffect(() => {
-    // void setDefaultCamera(camera.current);
-    camera.current.rotation.order = "YXZ";
-    camera.current.rotation.y = -Math.PI / 4;
-    camera.current.rotation.x = Math.atan(-1 / Math.sqrt(2));
-    camera.current.translateZ(100);
-
-    window.addEventListener("wheel", (e) => {
-      // Limit zoom dimensions
-      if (e.deltaY < 0 && camera.current.zoom <= aspect * 300)
-        setZoom(camera.current.zoom - (e.deltaY / 120) * 10);
-      if (e.deltaY > 0 && camera.current.zoom >= aspect * 50)
-        setZoom(camera.current.zoom - (e.deltaY / 120) * 10);
-    });
-  }, []);
-
-  return (
-    <OrthographicCamera
-      makeDefault
-      ref={camera}
-      zoom={zoom}
-      near={0.1}
-      far={500}
-      rotation={[Math.atan(-1 / Math.sqrt(2)), -Math.PI / 4, 0]}
-      onUpdate={(self) => self.updateProjectionMatrix()}
-      {...props}
-    />
   );
 };
 
 function App() {
-  const map = useMemo<KeyboardControlsEntry<Controls>[]>(
-    () => [
-      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
-      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
-      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
-      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
-      { name: Controls.jump, keys: ["Space"] },
-    ],
-    []
-  );
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <KeyboardControls map={map}>
-        <Canvas>
-          <Camera />
-          <ambientLight intensity={Math.PI / 2} />
-          <pointLight
-            position={[-10, -10, -10]}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
-        </Canvas>
-      </KeyboardControls>
+      <Canvas shadows camera={{ position: [0, 3, 5], fov: 60 }}>
+        <Comp />
+        <Sky
+          distance={450000}
+          sunPosition={[0, 1, 0]}
+          inclination={0}
+          azimuth={0.25}
+        />
+        <Ground />
+      </Canvas>
     </div>
   );
 }
